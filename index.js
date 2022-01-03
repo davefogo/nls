ebf99a389ee05d5998baa5dcc7a688983d2cc544
David Forero <davefogo@gmail.com>
2022-01-03 13:39:29 -0600

    final commit

--- index.js
+++ index.js
@@ -1,33 +1,46 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
+import fs from "fs";
+import util from "util";
+import chalk from "chalk";
+import path from "path";
 
-fs.readdir(process.cwd(), (err, filenames) => {
+// Method # 2
+// const lstat = util.promisify(fs.lstat);
+
+// Method # 3
+const { lstat } = fs.promises;
+const targetDir = process.argv[2] || process.cwd();
+
+fs.readdir(targetDir, async (err, filenames) => {
   if (err) {
     console.log(err);
   }
 
-  const allStats = Array(filenames.length).fill(null);
+  const statPromises = filenames.map(filename => {
+    return lstat(path.join(targetDir, filename));
+  })
 
-  for (let filename of filenames) {
-    const index = filenames.indexOf(filename);
+  const allStats = await Promise.all(statPromises);
 
-    fs.lstat(filename, (err, stats) => {
-      if (err) {
-        console.log(err);
-      }
-
-      allStats[index] = stats;
-
-      const ready = allStats.every((stats)=> {
-        return stats;
-      });
-
-      if (ready) {
-        allStats.forEach((stats, index) => {
-          console.log(filenames[index], stats.isFile());
-        });
-      }
-    })
+  for (let stats of allStats) {
+    const index = allStats.indexOf(stats);
+    if (stats.isFile()) {
+      console.log(filenames[index]);
+    } else {
+      console.log(chalk.hex('#FE019a').bold(filenames[index]));
+    }
   }
 });
+
+// Method #1
+// const lstat = (filename) => {
+//   return new Promise((resolve, reject) => {
+//     fs.lstat(filename, (err, stats) => {
+//       if (err) {
+//         reject(err);
+//       }
+//       resolve(stats);
+//     })
+//   });
+// };
